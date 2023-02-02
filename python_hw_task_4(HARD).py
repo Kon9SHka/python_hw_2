c9fb8fb16751beb3343af4534be8ba12752c1c45
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2023-02-02 17:19:06 +0300

    new file added

---
+++ python_hw_task_4(HARD).py
@@ -0,0 +1,7 @@
+#задача 4 НЕГА необязательная Задайте число. Составьте список чисел Фибоначчи, в том числе
+print("Hello")
+#k = int(input("Введите число: "))
+#max_index = k*2+1
+#fib_numbers = list()
+#fib_numbers.insert(k,0)
+#print(k)
